17a433d5d2cf3e991a2a964e440ccc6f55f3146b
herdeybayor <shereefdeen4real@gmail.com>
2025-09-10 06:24:24 +0100

    style: format code for consistency in index.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,9 +4,9 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
 app.listen(3000, () => {
-    console.log("Listening on port 3000!");
+  console.log("Listening on port 3000!");
 });
